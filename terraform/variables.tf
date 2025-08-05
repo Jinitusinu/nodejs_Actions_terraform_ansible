e7d2188338f8ff259ddb5bd283d8f86b97cc9978
Jinitusinu <georgejinitus@gmail.com>
2025-08-05 19:14:59 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -25,8 +25,3 @@ variable "public_key_path" {
   description = "Path to the public SSH key"
   type        = string
 }
-
-variable "public_key" {
-  description = "SSH public key content"
-  type        = string
-}
