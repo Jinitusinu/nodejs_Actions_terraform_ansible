90bd6384d09d7dce524da1481e8ef20df73cbd28
Jinitusinu <georgejinitus@gmail.com>
2025-08-05 20:42:28 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -65,7 +65,8 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "nodejs_app" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  key_name               = aws_key_pair.deployer.key_name
+  # key_name               = aws_key_pair.deployer.key_name
+  key_name      = "democentralcanda"  
   vpc_security_group_ids = [aws_security_group.node_sg.id]
 
   tags = {
