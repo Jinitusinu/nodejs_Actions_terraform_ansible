0d008ba2fe7025b97d53978199b3ae7828bb03d7
Jinitusinu <georgejinitus@gmail.com>
2025-08-05 23:18:29 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "public_ip" {
-  value = aws_instance.app_server.public_ip
+  value = aws_instance.nodejs_app.public_ip
 }
 
 output "ssh_command" {
-  value = "ssh -i ~/.ssh/democentralcanada.pem ubuntu@${aws_instance.app_server.public_ip}"
+  value = "ssh -i ~/.ssh/democentralcanada.pem ubuntu@${aws_instance.nodejs_app.public_ip}"
 }
-
-
