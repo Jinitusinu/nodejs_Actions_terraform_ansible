c493b1f11e52019a7bbe0ee6312aec5db4b34596
Saravanakumaran <60806451+saravanakumaran2@users.noreply.github.com>
2025-08-06 21:11:20 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,9 +6,7 @@ terraform {
     }
   }
 }
-########################################
-# Look up the default VPC in the region
-########################################
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -20,8 +18,8 @@ data "aws_key_pair" "existing" {
   key_name = var.key_name
 }
 
-resource "aws_security_group" "node_sg" {
-  name   = "node_sg1"
+resource "aws_security_group" "staging_node" {
+  name   = "staging_node"
   vpc_id = data.aws_vpc.default.id
 
   lifecycle {
@@ -32,7 +30,7 @@ ingress {
   description = "Allow all traffic"
   from_port   = 0
   to_port     = 0
-  protocol    = "-1"       # -1 means all protocols
+  protocol    = "-1"       
   cidr_blocks = ["0.0.0.0/0"]
 }
 
@@ -46,25 +44,25 @@ ingress {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical
+  owners      = ["099720109477"] 
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
-resource "aws_instance" "nodejs_app" {
+resource "aws_instance" "staging_node" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   key_name               = data.aws_key_pair.existing.key_name
-  vpc_security_group_ids = [aws_security_group.node_sg.id]
+  vpc_security_group_ids = [aws_security_group.staging_node.id]
 
   tags = {
-    Name = "nodejs-app"
+    Name = "staging_node"
   }
 }
 
-resource "aws_eip_association" "ansible_attach" {
-  instance_id   = aws_instance.nodejs_app.id
-  allocation_id = "eipalloc-0ae353f17264eefad"  # Replace this!
+resource "aws_eip_association" "staging_attach" {
+  instance_id   = aws_instance.staging_node.id
+  allocation_id = "eipalloc-0d5dac96c0b55b36f" 
 }
